26de1ec63f0f520c52facf090e7ce6af4afdab0b
francescocarrazzone <francescocarrazzone1@gmail.com>
2025-11-28 17:05:56 +0100

    feat: ending 02-strings

---
+++ 02-strings/script.js
@@ -0,0 +1,33 @@
+// 1. Dichiarazione di variabili
+// Crea una variabile chiamata name e assegnale il tuo nome.
+// Stampa il valore di nome nella console.
+
+// 2. Tipi primitivi
+// Crea tre variabili chiamate number, text e bool e assegna rispettivamente
+// un numero, una stringa e un valore booleano.
+// Stampa tutti i valori nella console.
+
+// 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
+// e le variabili sopra in modo da stampare correttamente 
+// in console la stringa con il nome completo
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
+const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
+const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
+
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName); // Output: "Jane Smith"
+
+// 4. Imposta questa variabile utilizzando una proprietà
+// sulla variabile alphabet per ottenere la sua lunghezza
+const numberOfLettersInAlphabet = `${alphabet.length}`;
+console.log(numberOfLettersInAlphabet); // Output: 26
+
+//1. dichiarazione di variabili
+const name = 'Mattia';
+console.log(name);
+
+//2. tipi primitivi
+let number = '13';
+let text = 'é il mio numero fortunato';
+let bool = 'true';
+console.log(number + text + bool)
